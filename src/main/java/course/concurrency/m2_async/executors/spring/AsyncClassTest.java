9aedd175feaff1efb6e6974b942ae7447c015778
Michael Mironov <nemiron94@gmail.com>
2023-10-11 19:19:04 +0300

    Make tasks asynchronous

--- src/main/java/course/concurrency/m2_async/executors/spring/AsyncClassTest.java
+++ src/main/java/course/concurrency/m2_async/executors/spring/AsyncClassTest.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
@@ -20,9 +21,8 @@ public class AsyncClassTest {
     @Async
     public void runAsyncTask() {
         System.out.println("runAsyncTask: " + Thread.currentThread().getName());
-        internalTask();
     }
-
+    @Async
     public void internalTask() {
         System.out.println("internalTask: " + Thread.currentThread().getName());
     }
---
+++ src/main/java/course/concurrency/m2_async/executors/spring/SpringAsyncConfig.java
@@ -0,0 +1,37 @@
+package course.concurrency.m2_async.executors.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class SpringAsyncConfig implements AsyncConfigurer
+{
+
+//    @Bean(name = "threadPoolTaskExecutor")
+//    public Executor asyncExecutor() {
+//        final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+//        executor.setCorePoolSize(4);
+//        executor.setMaxPoolSize(4);
+//        executor.setQueueCapacity(50);
+//        executor.setThreadNamePrefix("AsyncThread::");
+//        executor.initialize();
+//        return executor;
+//    }
+
+    @Override
+    public Executor getAsyncExecutor() {
+        final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(1);
+        executor.setThreadNamePrefix("AsyncThread::");
+        executor.initialize();
+        return executor;
+    }
+}
