153fda85e271c3571d3ae1ddcc254e4c38385212
Michael Mironov <nemiron94@gmail.com>
2023-10-15 20:05:29 +0300

    Add logger

--- src/main/java/course/concurrency/m2_async/executors/spring/AsyncClassTest.java
+++ src/main/java/course/concurrency/m2_async/executors/spring/AsyncClassTest.java
@@ -1,5 +1,7 @@
 package course.concurrency.m2_async.executors.spring;
 
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
@@ -12,6 +14,8 @@ import org.springframework.stereotype.Component;
 @EnableAsync
 public class AsyncClassTest {
 
+    private static final Logger log = LoggerFactory.getLogger(AsyncClassTest.class);
+
     @Autowired
     public ApplicationContext context;
 
@@ -21,10 +25,32 @@ public class AsyncClassTest {
 
     @Async
     public void runAsyncTask() {
-        System.out.println("runAsyncTask: " + Thread.currentThread().getName());
+        log.warn(() -> "*** RUN ***");
+        log.warn(() -> "Prefix - " + executor.getThreadNamePrefix());
+        log.warn(() -> "Class - " + executor.getClass());
+        log.warn(() -> "Active count - " + executor.getActiveCount());
+        log.warn(() -> "Core pool size - " + executor.getCorePoolSize());
+        log.warn(() -> "Max pool size" + executor.getMaxPoolSize());
+        log.warn(() -> "Keep alive - " + executor.getKeepAliveSeconds());
+        log.warn(() -> "Pool size - " + executor.getPoolSize());
+        log.warn(() -> "Executor type - " + executor.getThreadPoolExecutor());
+        log.warn(() -> "Thread group - " + executor.getThreadGroup());
+        log.warn(() -> "Thread priority - " + executor.getThreadPriority());
+        log.warn(() -> "runAsyncTask: " + Thread.currentThread().getName());
     }
     @Async
     public void internalTask() {
-        System.out.println("internalTask: " + Thread.currentThread().getName());
+        log.warn(() -> "*** ASYNC ***");
+        log.warn(() -> "Prefix - " + executor.getThreadNamePrefix());
+        log.warn(() -> "Class - " + executor.getClass());
+        log.warn(() -> "Active count - " + executor.getActiveCount());
+        log.warn(() -> "Core pool size - " + executor.getCorePoolSize());
+        log.warn(() -> "Max pool size" + executor.getMaxPoolSize());
+        log.warn(() -> "Keep alive - " + executor.getKeepAliveSeconds());
+        log.warn(() -> "Pool size - " + executor.getPoolSize());
+        log.warn(() -> "Executor type - " + executor.getThreadPoolExecutor());
+        log.warn(() -> "Thread group - " + executor.getThreadGroup());
+        log.warn(() -> "Thread priority - " + executor.getThreadPriority());
+        log.warn(() -> "internalTask: " + Thread.currentThread().getName());
     }
 }
